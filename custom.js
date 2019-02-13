ba6c34a8c7a05020dd4335ec8a129db8e94540b8
Monu Goyal <43906706+monugoyal@users.noreply.github.com>
2019-02-14 01:22:56 +0530

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
+    $('.customer-logos').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
